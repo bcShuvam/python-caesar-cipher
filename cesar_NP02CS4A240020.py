3872b0e160d9a07388fad97c0179537021d522b9
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2025-01-15 16:29:08 +0545

    Finished writing docstring and made changes in main method

--- cesar_NP02CS4A240020.py
+++ cesar_NP02CS4A240020.py
@@ -4,6 +4,10 @@
 import os
 
 def welcome():
+    """
+        This welcome function displays welcome message to the user,
+        and explains what this program is made for.
+    """
     print('Welcome to the Caesar Cipher')
     print('This program encrypts and decrypts text using Caesar Cipher.')
 
@@ -42,6 +46,31 @@ def enter_message():
             print('Invalid Choice')
 
 def encrypt(message, shift):
+    """
+        This function encrypts a message according to the shift number
+
+        Parameters:
+            message (str): The message which will be encrypted
+            shift (int): The number to shift each character forward of the message
+        
+        Returns:
+            str: The encrypted message
+        
+        Example:
+            >>> encrypt('Hello world', 4)
+            'LIPPS ASVPH'
+        
+        How it works:
+            suppose ascii value => ord('H') -> returns 72
+            and shift number is 4
+            algorithm:
+            = ((asciiVal - 65 + shift) % 26) + 65
+            = ((72 - 65 + 4) % 26) + 65
+            = (11 % 26) + 65
+            = 11 + 65
+            = 76
+            chr(76) -> returns 'L'
+    """
     encryptedMsg = ''
     for char in message:
         asciiVal = ord(char)
@@ -61,6 +90,22 @@ def encrypt(message, shift):
     return encryptedMsg
 
 def decrypt(message, shift):
+    """
+        This function decrypts a message according to the shift number.
+        It algorithm for decryption is similar as encryption 
+        but in decryption instead of adding the shift number we subtract it.
+
+        Parameters:
+            message (str): The message which will be decrypted
+            shift (int): The number to shift each character backward of the message
+        
+        Returns:
+            str: The encrypted message
+        
+        Example:
+            >>> encrypt('LIPPS ASVPH', 4)
+            'HELLO WORLD'
+    """
     decryptedMsg = ''
     for char in message:
         asciiVal = ord(char)
@@ -79,6 +124,26 @@ def decrypt(message, shift):
     return decryptedMsg
 
 def process_file(filename, mode):
+    """
+        This function reads all the contents from the given filename.
+        And calls encrypt() or decrypt() function according to the mode received from parameter.
+        Then it will call the write_message(messages) function and pass list of message to it,
+        and produce output to result to results.txt file.
+
+        This function will prompt the user to enter shift number again and again,
+        and checks if the shift number is an valid integer of not using while loop.
+
+        Parameters:
+            filename (str): The name of the file which contains the messages
+            mode (str): The mode of operation (e for encrypt, d for decrypt)
+
+        Returns:
+            None
+        
+        Example:
+            >>> process_file('input.txt', 'e')
+            encrypts all the messages in 'input.txt' and saves them in'results.txt'
+    """
     shift = 0
     messageList = []
     with open(filename, 'r') as file:
@@ -100,9 +165,26 @@ def process_file(filename, mode):
     write_message(messageList)
 
 def is_file(filename):
+    """
+        This function return boolean value to the user.
+
+        Parameters:
+            (str) as filename
+
+        Returns:
+            This function uses python's os module to verify if file exists or not.
+            It will return True if the file exists and
+            returns False if the file doses not exist.
+    """
     return os.path.isfile(filename)
 
 def write_message(messages):
+    """
+        This function writes the list of messages to results.txt file.
+
+        This function requires one parameter as a string or list of strings
+        The opens a file named results.txt
+    """
     with open('results.txt', 'w') as file:
         for msg in messages:
             file.write(f'{msg}\n')
@@ -110,6 +192,16 @@ def write_message(messages):
     
 
 def message_or_file():
+    """
+        This function prompts the user to enter values as asked in the console.
+
+        This function uses while loop to check if the user correct input or not,
+        and calls the respective functions according to the input values entered by user.
+
+        At the end the function will ask the user to exit or continue cesar cipher the program.
+        If the user enters (e) to exit, the program will terminate, 
+        and if user enters (y) to continue then it will continue the program.
+    """
     mode = ''
     fileName = ''
     readFromFileOrConsole = ''
@@ -159,6 +251,13 @@ def message_or_file():
         else:
             print('Invalid Choice')
 
-if __name__ == '__main__':
+def main():
+    """
+        This main function is calls welcome and message_or_file() function.
+        It will run the whole cesar cipher program.
+    """
     welcome()
     message_or_file()
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+import cesar_NP02CS4A240020 as cesar
+
+cesar.welcome()
+# cesar.main()
+# cesar.main()
+# cesar.main()
